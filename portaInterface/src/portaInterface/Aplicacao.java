5de220a072932aa186d3b80311dbdb1f4e959f2f
Tiego <tiegoparotivo@gmail.com>
2016-05-02 19:02:08 -0300

    Padronizando comentários

--- portaInterface/src/portaInterface/Aplicacao.java
+++ portaInterface/src/portaInterface/Aplicacao.java
@@ -3,21 +3,25 @@ package portaInterface;
 public class Aplicacao {
 
 	public static void main(String[] args) throws Exception {
-		// Cria um objeto porta, passando como parâmetro sua altura e sua largura.
-		Porta p1 = new Porta (2.1, 0.9);
+		/* Cria um objeto porta, passando como 
+		 * parâmetro sua altura e sua largura.
+		 */
+		Porta p1 = new Porta(2.1, 0.9);
 		
-		// chamada do método abrir, no qual abre uma porta.
+		/* chamada do método abrir, no qual abre uma porta.*/
 		p1.abrir();
 		
-		// Saída com as informações de altura, largura e se a porta está aberta.
+		/* Saída com as informações de altura, 
+		 * largura e se a porta está aberta.
+		 */
 		System.out.println("p1 -> Altura = " + p1.getAltura());
 		System.out.println("p1 -> Largura = " + p1.getLargura());
 		System.out.println("p1 -> Aberta? = " + p1.isAberta());
 		
-		// Cópia do objeto acima, com os mesmos valores para os atributos.
-		// Realiza um cast explícito para o tipo "Porta".
+		/* Cópia do objeto acima, com os mesmos valores para os atributos.
+		 * Realiza um cast explícito para o tipo "Porta".
+		 */
 		Porta p2 = (Porta) p1.clone();
-		
 		System.out.println("\np2 -> Altura = " + p2.getAltura());
 		System.out.println("p2 -> Largura = " + p2.getLargura());
 		System.out.println("p2 -> Aberta? = " + p2.isAberta());
--- portaInterface/src/portaInterface/Porta.java
+++ portaInterface/src/portaInterface/Porta.java
@@ -1,50 +1,62 @@
 package portaInterface;
 
-// Implementa a interface Cloneable que permite criar um novo objeto com base no objeto original.
+   /*Implementa a interface Cloneable, 
+    * que permite criar um novo objeto com base no objeto original. 
+	*/ 
 public class Porta implements Cloneable {
 	
-	//Atributos que armazenam altura, largura e se a porta está aberta ou não, respectivamente.
+	/* Atributos que armazenam altura, 
+	 * largura e se a porta está aberta ou não, respectivamente.
+	 */
+	private double altura = 0.0;
+	private double largura = 0.0;
+	private boolean aberta = false;
 	
-	private double altura;
-	private double largura;
-	private boolean aberta;
-	
-	// Construtor que recebe a altura e a largura de uma porta quando esse objeto for criado.
-	public Porta (double altura, double largura){
+	/* Construtor que recebe a altura e a largura de uma porta 
+	 * quando esse objeto for criado.
+	 */
+	public Porta(double altura, double largura){
 		this.altura = altura;
 		this.largura = largura;
 	}
 	
-	// Método que abre a porta.
+	/* Método que abre a porta. */
 	public void abrir(){
 		aberta = true;
 	}
 	
-	// Método que fecha a porta.
+	/* Método que fecha a porta. */
 	public void fechar(){
 		aberta = false;	
 	}
 	
-	// Método get que permite acessar o atributo altura de um objeto "porta".
+	/* Método get que permite acessar 
+	 * o atributo altura de um objeto "porta". 
+	 */
 	public double getAltura() {
 		return altura;
 	}
 	
-	// Método get que permite acessar o atributo largura de um objeto "porta".
+	/* Método get que permite acessar 
+	 * o atributo Largura de um objeto "porta". 
+	 */
 	public double getLargura() {
 		return largura;
 	}
 	
-	// Método get que permite acessar o atributo aberta de um objeto "porta".
+	/* Método get que permite acessar 
+	 * o atributo aberta de um objeto "porta".
+	 */
 	public boolean isAberta() {
 		return aberta;
 	}
 	
-	// Método da classe Object que é sobrescrito para criar uma cópia de um objeto original (porta).
+	/* Método da classe Object que é sobrescrito 
+	 * para criar uma cópia de um objeto original (porta).
+	 */
 	public Object clone() throws CloneNotSupportedException{
 		Porta p = new Porta(this.altura, this.largura);
 		p.aberta = this.aberta;
-		
 		return p;
 	}
 }
